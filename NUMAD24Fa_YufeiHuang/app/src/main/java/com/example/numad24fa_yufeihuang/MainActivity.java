4309503f9db1678617c5c374b5e3b03591c29c9f
yufei-ilariahuang <fayyyufei@gmail.com>
2024-09-18 10:50:16 -0700

    Added NUMAD24Fa_YufeiHuang project to the MobileApp repository

---
+++ NUMAD24Fa_YufeiHuang/app/src/main/java/com/example/numad24fa_yufeihuang/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.numad24fa_yufeihuang;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the TextView and Button by their IDs
+        TextView textView = findViewById(R.id.textView);
+        Button displayButton = findViewById(R.id.display_button);
+
+        // Set OnClickListener on the button to display a Toast with name and email
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hardcoded name and email for demonstration
+                String name = "Yufei Huang";
+                String email = "huang.yufe@northeastern.edu";
+
+                // Create the toast message
+                String message = "Name: " + name + "\nEmail: " + email;
+
+                // Display the toast
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
